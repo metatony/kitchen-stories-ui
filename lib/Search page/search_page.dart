7da655947f6284ad98d8ff6288784b06374778b9
alexacolbe <ibegbulam.ua@gmail.com>
2023-01-11 14:21:39 +0100

    modified empty pagees

--- lib/Search page/search_page.dart
+++ lib/Search page/search_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text('Coming Soon'),
+      ),
     );
   }
 }
--- lib/Shopping page/shopping_page.dart
+++ lib/Shopping page/shopping_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class ShoppingPage extends StatelessWidget {
   const ShoppingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text('Coming Soon'),
+      ),
     );
   }
 }
