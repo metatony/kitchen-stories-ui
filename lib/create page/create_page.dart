e147b1b2832b18b1098dbd2466a5385153a38b40
alexacolbe <ibegbulam.ua@gmail.com>
2023-01-11 14:33:29 +0100

    ...

--- lib/create page/create_page.dart
+++ lib/create page/create_page.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class CreatePage extends StatelessWidget {
   const CreatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
+    return Container(
+      color: Colors.white,
+      child: const Center(
+        child: Text('Coming Soon'),
+      ),
     );
   }
 }
